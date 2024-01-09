1b6d3cf51d231c630800fa0f40f5a4888341d9d8
Mahdi dalvandi <mahdi.da128@gmail.com>
2024-01-09 12:20:25 +0330

    5pp

--- src/components/popUp/popUp.jsx
+++ src/components/popUp/popUp.jsx
@@ -1,3 +1,4 @@
+// PopupContent.js
 import React, { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import * as echarts from "echarts";
@@ -5,15 +6,12 @@ import * as echarts from "echarts";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 function PopupContent({ chartOptions, selectedZone, nearbyPoints, onClose }) {
-  const [isOpen, setIsOpen] = useState(true);
-
   const chartContainerRef = useRef(null);
   const chartInstance = useRef(null);
   const mapContainerRef = useRef(null);
   const mapInstance = useRef(null);
-  const handleClose = () => {
-    setIsOpen(false);
-  };
+  const [isOpen, setIsOpen] = useState(true);
+
   useEffect(() => {
     // Initialize ECharts chart
     chartInstance.current = echarts.init(chartContainerRef.current);
@@ -37,59 +35,63 @@ function PopupContent({ chartOptions, selectedZone, nearbyPoints, onClose }) {
       new mapboxgl.Marker({ color: "#ff8000" })
         .setLngLat([point.lng, point.lat])
         .addTo(mapInstance.current);
-      console.log([point.lng, point.lat]);
     });
 
-    return () => {
+    // Cleanup function for previous instances
+    const cleanup = () => {
       if (chartInstance.current) {
         chartInstance.current.dispose();
       }
-      if (mapInstance.current) {
+
+      // Check if the map container element is still in the DOM
+      const mapContainer = mapContainerRef.current;
+      if (mapContainer && mapContainer.parentNode) {
         mapInstance.current.remove();
       }
     };
+
+    // Attach cleanup function to the component unmount
+    return cleanup;
   }, [chartOptions, selectedZone, nearbyPoints]);
 
   return (
     <>
-      {isOpen && (
-        <div className="fixed w-5/6 h-4/5 m-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 z-50">
-          <div className="flex h-fit">
-            <div className=" flex-col inline-grid h-fit text-nowrap">
-              <div className=" pb-8 h-fit w-fit">
-                <div
-                  ref={mapContainerRef}
-                  style={{ width: "250px", height: "250px" }}
-                />
-              </div>
-              <h3>
-                Selected Zone:{" "}
-                <p className="font-semibold text-nowrap inline-flex">
-                  {selectedZone.name}
-                </p>
-              </h3>
-              <p className="font-bold mt-3">Nearby Points:</p>
-              <ul>
-                {nearbyPoints.map((point, index) => (
-                  <li key={index}>
-                    Name: {point.name}, Distance: {point.distance} km
-                  </li>
-                ))}
-              </ul>
+      <div className="fixed w-5/6 h-4/5 m-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 z-50">
+        <div className="flex h-fit">
+          <div className=" flex-col inline-grid h-fit text-nowrap">
+            <div className=" pb-8 h-fit w-fit">
+              <div
+                ref={mapContainerRef}
+                style={{ width: "250px", height: "250px" }}
+              />
             </div>
-            <div className="w-3/5 p-1 mt-12 ml-5">
-              <div ref={chartContainerRef} style={{ height: "380px" }} />
-            </div>
+            <h3>
+              Selected Zone:{" "}
+              <p className="font-semibold text-nowrap inline-flex">
+                {selectedZone.name}
+              </p>
+            </h3>
+            <p className="font-bold mt-3">Nearby Points:</p>
+            <ul>
+              {nearbyPoints.map((point, index) => (
+                <li key={index}>
+                  Name: {point.name}, Distance: {point.distance} km
+                </li>
+              ))}
+            </ul>
           </div>
-
-          <button
-            className="absolute top-2 right-2 bg-gray-300 px-4 py-2 rounded"
-            onClick={handleClose}
-          >
-            Close
-          </button>
+          <div className="w-3/5 p-1 mt-12 ml-5">
+            <div ref={chartContainerRef} style={{ height: "380px" }} />
+          </div>
         </div>
-      )}
+
+        <button
+          className="absolute top-2 right-2 bg-gray-300 px-4 py-2 rounded"
+          onClick={onClose}
+        >
+          Close
+        </button>
+      </div>
     </>
   );
 }
